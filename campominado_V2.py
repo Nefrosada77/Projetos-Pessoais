12f736652d18ff105b9016fc44778b517a754e0b
Nefrosada77 <143364899+Nefrosada77@users.noreply.github.com>
2024-02-09 00:26:21 -0300

    Update campominado_V2.py

--- campominado_V2.py
+++ campominado_V2.py
@@ -1,12 +1,45 @@
 import PySimpleGUI as sg
+import random
 tamanho = 6  
-layout = [[sg.Button(f'{a}{b}',size=(4,2),button_color="LightGrey",disabled_button_color=("DarkGrey","Grey")) for b in range(tamanho)]for a in range(tamanho)]
+bombas = 4
+layout = [[sg.Button(button_text=f'{a}{b}',size=(4,2),button_color="LightGrey",disabled_button_color=("DarkGrey","Grey")) for b in range(tamanho)]for a in range(tamanho)]
+for num in range(bombas):
+    linha = [f'{l}' for l in range(tamanho)]
+    coluna = [f'{c}' for c in range(tamanho)]
+    lin = int(random.choice(linha))
+    col = int(random.choice(coluna))
+    if layout[lin][col].get_text() != "*":
+        layout[lin][col] = sg.Button(button_text='*',size=(4,2),button_color="LightGrey",disabled_button_color=("DarkGrey","Grey"))
+    else:
+        num -= 1
+    
 window = sg.Window('Campo Minado',layout,resizable=True,element_justification="Center")
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
+        break   
+    if event == '*':
+        print("Game Over")
         break
-    window[f'{event}'].update(disabled=True)
-    
-window.close()
+    else:
+        cavacao = True   
+        y = int(event[0])
+        x = int(event[1])
+        contador = 0
+        for l in range(y-1,y+2):
+            for c in range(x-1,x+2):
+                if (((l >= tamanho) or (l < 0)) or ((c >= tamanho) or (c < 0))):
+                    pass                    
+                elif layout[l][c].get_text() == '*':
+                    contador += 1
+                else:
+                    pass
+        window[f'{event}'].update(text=f'{contador}',disabled=True)
 
+                            
+                
+
+
+
+
+window.close()
