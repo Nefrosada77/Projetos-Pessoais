9a07fd7c827902d01dfb6ee9fc6b5acbc54823fb
Gabriel Siciliani <bibielsmo@gmail.com>
2024-03-22 15:01:49 -0300

    Update CodeBreakerV1.py

--- Py/CodeBreakerV1.py
+++ Py/CodeBreakerV1.py
@@ -1,5 +1,7 @@
 import random  
 import PySimpleGUI as sg
+
+sg.SetOptions(auto_size_buttons=False,button_element_size=(6,3),button_color=('DarkRed','Grey'),text_element_background_color='Grey',text_justification='Center')
 '''-----------------------------------------------------------'''
 #CRIAÇÃO DA MATRIX DOS NUMEROS QUE SERÃO SELECIONADOS
 NUM = []
@@ -20,66 +22,75 @@ for linha in GRID:
     print(linha)
 '''-----------------------------------------------------------'''
 #JOGO
-LAYOUTGAME = [[sg.Button(button_text=1) for x in range(3)]for y in range(3)]
-window = sg.Window('Code Breaker V.1',LAYOUTGAME)
+LAYOUTGAME = [[sg.Text('Turno: 1',key='Cont')],
+            [sg.Button(button_text='1',key=0),sg.Button(button_text=1,key=1),sg.Button(button_text=1,key=2),sg.Text(text='0',text_color='White',background_color='Orange',size=(2,3),key=('H0')),sg.Text(text='0',text_color='White',background_color='Green',size=(2,3),key=('B0'))],
+            [sg.Button(button_text='1',key=10),sg.Button(button_text=1,key=11),sg.Button(button_text=1,key=12),sg.Text(text='0',text_color='White',background_color='Orange',size=(2,3),key=('H1')),sg.Text(text='0',text_color='White',background_color='Green',size=(2,3),key=('B1'))],
+            [sg.Button(button_text='1',key=20),sg.Button(button_text=1,key=21),sg.Button(button_text=1,key=22),sg.Text(text='0',text_color='White',background_color='Orange',size=(2,3),key=('H2')),sg.Text(text='0',text_color='White',background_color='Green',size=(2,3),key=('B2'))],
+            [sg.Text(text='0',text_color='White',background_color='Orange',size=(5,1),key=('H3')),sg.Text(text='0',text_color='White',background_color='Orange',size=(5,1),key=('H4')),sg.Text(text='0',text_color='White',background_color='Orange',size=(5,1),key=('H5'))],
+            [sg.Text(text='0',text_color='White',background_color='Green',size=(5,1),key=('B3')),sg.Text(text='0',text_color='White',background_color='Green',size=(5,1),key=('B4')),sg.Text(text='0',text_color='White',background_color='Green',size=(5,1),key=('B5'))],            
+            [sg.Button('CONFIRM',size=(16,2),button_color="DarkRed on Grey")]]
+window = sg.Window('Code Breaker V.1',LAYOUTGAME,element_justification='center',resizable=True,background_color="DarkGrey",scaling=3,text_justification='center')
+turno = 1
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         window.close()
         break
     if event != 'CONFIRM':
-        match int(event):
-            case 1:
-                but = int(LAYOUTGAME[0][0].get_text())
+                but = int(window[event].get_text())
                 if but != 9:
-                    LAYOUTGAME[0][0].update(but+1)
+                    window[event].update(but+1)
                 else:
-                    LAYOUTGAME[0][0].update(1)
-            case 10:
-                but = int(LAYOUTGAME[0][1].get_text())
-                if but != 9:
-                    LAYOUTGAME[0][1].update(but+1)
+                    window[event].update(1)
+    else:
+        Vitoria = 0
+        for a in range(0,30,10):
+            HitH = 0
+            BeH = 0
+            if window[a].get_text() in GRID[int(a/10)]:
+                if window[a].get_text() == GRID[int(a/10)][0]:
+                    HitH += 1
                 else:
-                    LAYOUTGAME[0][1].update(1)
-            case 11:
-                but = int(LAYOUTGAME[0][2].get_text())
-                if but != 9:
-                    LAYOUTGAME[0][2].update(but+1)
+                    BeH += 1
+            if window[a+1].get_text() in GRID[int(a/10)]:
+                if window[a+1].get_text() == GRID[int(a/10)][1]:
+                    HitH += 1
                 else:
-                    LAYOUTGAME[0][2].update(1)
-            case 12:
-                but = int(LAYOUTGAME[1][0].get_text())
-                if but != 9:
-                    LAYOUTGAME[1][0].update(but+1)
+                    BeH += 1
+            if window[a+2].get_text() in GRID[int(a/10)]:
+                if window[a+2].get_text() == GRID[int(a/10)][2]:
+                    HitH += 1
                 else:
-                    LAYOUTGAME[1][0].update(1)
-            case 13:
-                but = int(LAYOUTGAME[1][1].get_text())
-                if but != 9:
-                    LAYOUTGAME[1][1].update(but+1)
-                else:
-                    LAYOUTGAME[1][1].update(1)
-            case 14:
-                but = int(LAYOUTGAME[1][2].get_text())
-                if but != 9:
-                    LAYOUTGAME[1][2].update(but+1)
-                else:
-                    LAYOUTGAME[1][2].update(1)
-            case 15:
-                but = int(LAYOUTGAME[2][0].get_text())
-                if but != 9:
-                    LAYOUTGAME[2][0].update(but+1)
-                else:
-                    LAYOUTGAME[2][0].update(1)
-            case 16:
-                but = int(LAYOUTGAME[2][1].get_text())
-                if but != 9:
-                    LAYOUTGAME[2][1].update(but+1)
-                else:
-                    LAYOUTGAME[2][1].update(1)
-            case 17:
-                but = int(LAYOUTGAME[2][2].get_text())
-                if but != 9:
-                    LAYOUTGAME[2][2].update(but+1)
-                else:
-                    LAYOUTGAME[2][2].update(1)
+                    BeH += 1
+            if HitH == 3:
+                Vitoria += 1
+            window[f'H{int(a/10)}'].update(HitH)
+            window[f'B{int(a/10)}'].update(BeH)
+        for a in range(0,3,1):
+            HitV = 0
+            BeV = 0
+            if window[a].get_text() == GRID[0][a]:
+                HitV += 1
+            else:
+                BeV += 1
+            if window[a+10].get_text() == GRID[1][a]:
+                HitV += 1
+            else:
+                BeV += 1
+            if window[a+20].get_text() == GRID[2][a]:
+                HitV += 1
+            else:
+                BeV += 1
+            if HitV == 3:
+                Vitoria += 1
+            window[f'H{a+3}'].update(HitV)
+            window[f'B{a+3}'].update(BeV)
+        turno += 1
+        window['Cont'].update(f'Turno: {turno}')
+        if turno == 8:
+            window.close()
+            break
+        else:
+            if Vitoria == 3:
+                window.close()
+                break
